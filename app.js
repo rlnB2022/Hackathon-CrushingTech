64c66b402c6a1f65f832d3297686579ce39c379a
rlnB2022 <beyer.richard73@gmail.com>
2023-11-20 17:50:49 -0600

    Added toggle to setup guide

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 /* Setup guide */
 const setupGuideToggle = document.getElementById("setup-guide-toggle");
+const guideImage = document.querySelector("#setup-guide-toggle img");
 
 /**
  * Toggle Guide selector
  */
 const toggleGuide = () => {
-	if (setupGuideToggle.classList.contains("active")) {
-		setupGuideToggle.classList.remove("active");
-	} else {
-		setupGuideToggle.classList.add("active");
-	}
+	const imageArray = [
+		"https://crushingit.tech/hackathon-assets/icon-arrow-down.svg",
+		"https://crushingit.tech/hackathon-assets/icon-arrow-up.svg",
+	];
+
+	let currentImg = parseInt(guideImage.dataset.num);
+	let nextImg = currentImg === 0 ? 1 : 0;
+	guideImage.src = imageArray[nextImg];
+	guideImage.dataset.num = nextImg;
 };
 
 setupGuideToggle.addEventListener("click", toggleGuide);
