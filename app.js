7391fd2fd525540937f376cb411e35e62b121c74
rlnB2022 <beyer.richard73@gmail.com>
2023-11-20 20:14:02 -0600

    Toggle now works.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const guideImage = document.querySelector("#setup-guide-toggle img");
  * Toggle Guide selector
  */
 const toggleGuide = () => {
+	const innerGuide = document.querySelector(".inner-guide");
+
 	const imageArray = [
 		"https://crushingit.tech/hackathon-assets/icon-arrow-down.svg",
 		"https://crushingit.tech/hackathon-assets/icon-arrow-up.svg",
@@ -15,6 +17,9 @@ const toggleGuide = () => {
 	let nextImg = currentImg === 0 ? 1 : 0;
 	guideImage.src = imageArray[nextImg];
 	guideImage.dataset.num = nextImg;
+
+	// set active class on .inner-guide so the accordion will display/hide
+	innerGuide.classList.toggle("active");
 };
 
 setupGuideToggle.addEventListener("click", toggleGuide);
